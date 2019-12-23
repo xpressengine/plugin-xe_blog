8e898b47fe47254cf60d818e77bbb1824888e05e
sirwoongke <fzwoongbin@naver.com>
2019-12-23 11:44:47 +0900

    init plugin

---
+++ src/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Post\XePlugin\Post;
+
+use XeFrontend;
+use XePresenter;
+use App\Http\Controllers\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    public function index()
+    {
+        $title = 'Post plugin';
+
+        // set browser title
+        XeFrontend::title($title);
+
+        // load css file
+        XeFrontend::css(Plugin::asset('assets/style.css'))->load();
+
+        // output
+        return XePresenter::make('post::views.index', ['title' => $title]);
+    }
+}
