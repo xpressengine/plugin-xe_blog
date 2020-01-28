d8c8654971310a439b9a8111ff09d414e455260c
leejihyeok <bebop104@xpressengine.com>
2020-01-28 16:07:41 +0900

    블럭에디터 - 편집 페이지 하단 다이나믹필드 영역 마크업 추가 (마크업 페이지가 따로 없어서 blade 파일에서 작업, 개발 적용 시 제거 필요)

--- views/blog/edit.blade.php
+++ views/blog/edit.blade.php
@@ -1,3 +1,4 @@
+
 <form method="post" action="{{ route('blog.update') }}">
     {!! csrf_field() !!}
 
@@ -34,3 +35,73 @@
 
     <button type="submit" class="xe-btn">저장</button>
 </form>
+
+<!-- 개발 적용 후 삭제 필요 -->
+<form>
+    <!-- blog 플러그인 -->
+    <section class="section-blog-block-editor-field">
+        <div class="blog-block-editor-field__title-box">
+            <h2 class="blog-block-editor-field__title">사용자 정의 필드</h2>
+        </div>
+        <div class="blog-block-editor-filed-content">
+            <div class="inner">
+                <!-- 다이나믹 필드 추가 영역 (스타일은 boldjournal의 Skins/DynamicField/WriterInfomationSkin/assets 에 있음) -->
+
+                <div class="bold-block-editor-field">
+                    <div class="bold-block-editor-field-content">
+                        <table class="bold-block-editor-field-table">
+                            <colgroup>
+                                <col style="width: 33.3333%">
+                                <col style="width: 66.6666%">
+                            </colgroup>
+                            <thead>
+                                <tr>
+                                    <th>이름</th>
+                                    <th>값</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="bold-vertical-top">
+                                        <label class="blind" for="item1test1">이름</label>
+                                        <input type="text" id="item1test1" class="bold-block-editor-field__input" placeholder="이름을 입력하세요." value="subtitle" />
+                                        <div class="bold-block-editor-field__button-box">
+                                            <button type="button" class="bold-block-editor-field__button">삭제</button>
+                                        </div>
+                                    </td>
+                                    <td class="bold-vertical-top">
+                                        <label class="blind" for="item1test2">값</label>
+                                        <input type="text" id="item1test2" class="bold-block-editor-field__input" placeholder="내용을 입력하세요." value="남편이 게임중독이라구요?" />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="bold-vertical-top">
+                                        <label class="blind" for="item2test1">이름</label>
+                                        <input type="text" id="item2test1" class="bold-block-editor-field__input" placeholder="이름을 입력하세요." value="subtitle" />
+                                        <div>
+                                            <button type="button" class="bold-block-editor-field__button">삭제</button>
+                                        </div>
+                                    </td>
+                                    <td class="bold-vertical-top">
+                                        <label class="blind" for="item2test2">이름</label>
+                                        <input type="text" id="item2test2" class="bold-block-editor-field__input" placeholder="내용을 입력하세요." value="" />
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="bold-block-editor-field__more-button-box">
+                        <button type="button" class="bold-block-editor-field__button bold-block-editor-field__button--more">
+                            <svg aria-hidden="true" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" class="dashicon dashicons-insert"><path d="M10 1c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7zm1-11H9v3H6v2h3v3h2v-3h3V9h-3V6z"></path></svg>
+                            <span class="bold-block-editor-field__button--more-text">사용자 정의 필드추가</span>
+                        </button>
+                    </div>
+                </div>
+
+                <!-- //다이나믹 필드 추가 영역 -->
+            </div>
+        </div>
+    </section>
+    <!-- //blog 플러그인 -->
+</form>
+<!-- //개발 적용 후 삭제 필요 -->
