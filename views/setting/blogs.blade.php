b39abc6a0051ca72ef6bfcd645a276c0c32ce5d4
sirwoongke <fzwoongbin@naver.com>
2020-01-30 16:36:46 +0900

    글 관리 목록 정리

--- views/setting/blogs.blade.php
+++ views/setting/blogs.blade.php
@@ -1,48 +1,67 @@
-<a href="{{ route('blog.create') }}" class="xe-btn xe-btn-positive" target="_blank">생성</a>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="panel-group">
+            <div class="panel">
+                <div class="panel-heading">
+                    <div class="pull-left">
+                        <h3 class="panel-title">{{ xe_trans('xe_blog::manageBlog') }}</h3>
+                    </div>
 
-<table class="xe-table">
-    <thead>
-    <tr>
-        <th>제목</th>
-        <th>작성자</th>
-        @foreach ($taxonomies as $taxonomy)
-            <th>{{ xe_trans($taxonomy->name) }}</th>
-        @endforeach
-        <th>생성일</th>
-        <th>수정일</th>
-        <th>발행일</th>
-    </tr>
-    </thead>
-    <tbody>
-        @foreach($blogs as $blog)
-            <tr>
-                <td @if ($metaDataHandler->getBackgroundColor($blog) !== null) style="background-color: {{ $metaDataHandler->getBackgroundColor($blog) }}" @endif>
-                    <a href="{{ route('blog.show', ['id' => $blog->id]) }}" target="_blank">
-                        @if ($metaDataHandler->getThumbnail($blog) !== null)
-                            <img src="{{ $metaDataHandler->getThumbnail($blog, 'spill', 'S')->url() }}">
-                        @else
-                            {{ $blog->title }}
-                        @endif
-                    </a>
-                </td>
-                <td>
-                    @if ($blog->user !== null)
-                        {{ $blog->user->getDisplayName() }}
-                    @else
-                        guest
-                    @endif
-                </td>
-                @foreach ($taxonomies as $taxonomy)
-                    @if ($taxonomyHandler->getBlogTaxonomyItem($blog, $taxonomy->id) !== null)
-                        <td>{{ xe_trans($taxonomyHandler->getBlogTaxonomyItem($blog, $taxonomy->id)->word) }}</td>
-                    @else
-                        <td></td>
-                    @endif
-                @endforeach
-                <td>{{ $blog->created_at->format('Y-m-d H:i:s') }}</td>
-                <td>{{ $blog->updated_at->format('Y-m-d H:i:s') }}</td>
-                <td>{{ $blog->published_at->format('Y-m-d H:i:s') }}</td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+                    <div class="pull-right">
+                        <a href="{{ route('blog.create') }}" class="xe-btn xe-btn-positive" target="_blank">생성</a>
+                    </div>
+                </div>
+
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>제목</th>
+                            <th>작성자</th>
+                            @foreach ($taxonomies as $taxonomy)
+                                <th>{{ xe_trans($taxonomy->name) }}</th>
+                            @endforeach
+                            <th>생성일</th>
+                            <th>수정일</th>
+                            <th>발행일</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($blogs as $blog)
+                            <tr>
+                                <td>
+                                    <a href="{{ route('blog.show', ['id' => $blog->id]) }}" target="_blank">{{ $blog->title }}</a>
+                                </td>
+                                <td>
+                                    @if ($blog->user !== null)
+                                        {{ $blog->user->getDisplayName() }}
+                                    @else
+                                        guest
+                                    @endif
+                                </td>
+                                @foreach ($taxonomies as $taxonomy)
+                                    @if ($taxonomyHandler->getBlogTaxonomyItem($blog, $taxonomy->id) !== null)
+                                        <td>{{ xe_trans($taxonomyHandler->getBlogTaxonomyItem($blog, $taxonomy->id)->word) }}</td>
+                                    @else
+                                        <td></td>
+                                    @endif
+                                @endforeach
+                                <td>{{ $blog->created_at->format('Y-m-d H:i:s') }}</td>
+                                <td>{{ $blog->updated_at->format('Y-m-d H:i:s') }}</td>
+                                <td>{{ $blog->published_at->format('Y-m-d H:i:s') }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="panel-footer">
+                    <div class="pull-left">
+                        <nav>
+                            {!! $blogs->render() !!}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
