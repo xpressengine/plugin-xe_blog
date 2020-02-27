97ce6d06f0d3fa7adf15b06faca42dc4d415c05f
bnu <bnufactory@gmail.com>
2020-02-27 12:22:11 +0900

    글쓰기시 이미지 첨부 안 되는 문제 고침

--- views/blog/create.blade.php
+++ views/blog/create.blade.php
@@ -116,13 +116,13 @@
             <div class="components-base-control">
                 <div class="components-base-control__field">
                     <span class="components-base-control__label">썸네일</span>
-                    {!! uio('formMedialibraryImage', [ 'name' => 'thumbnail', 'limit' => 1 ]) !!}
+                    {!! uio('formMedialibraryImage', [ 'name' => 'thumbnail', 'field' => '#metaboxes [name=thumbnail]', 'limit' => 1 ]) !!}
                 </div>
             </div>
             <div class="components-base-control">
                 <div class="components-base-control__field">
                     <span class="components-base-control__label">커버 이미지</span>
-                    {!! uio('formMedialibraryImage', [ 'name' => 'cover_image', 'limit' => 1 ]) !!}
+                    {!! uio('formMedialibraryImage', [ 'name' => 'cover_image', 'field' => '#metaboxes [name=thumbnail]', 'limit' => 1 ]) !!}
                 </div>
             </div>
         </div>
