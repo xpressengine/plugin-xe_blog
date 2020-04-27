d252f41a76fc1d76bdc5c8db9836efd2feb9173f
sirwoongke <fzwoongbin@naver.com>
2020-04-27 17:24:13 +0900

    사용하지 않는 기능 제거

--- src/Components/Skins/Blog/BlogCommonSkin/views/create.blade.php
+++ src/Components/Skins/Blog/BlogCommonSkin/views/create.blade.php
@@ -1,11 +1,11 @@
 {{ XeFrontend::css('plugins/xe_blog/assets/block-editor-dynamic-fields.css')->load() }}
 {{ XeFrontend::js('plugins/xe_blog/assets/js/boldjournal-block-style.js')->load() }}
 
-@expose_route('boldjournal.gallery.edit_banner_group')
+@expose_route('banner::group.edit')
 
 <form method="post" class="metabox-base-form" action="{{ route('blog.store') }}" enctype="multipart/form-data">
     {!! csrf_field() !!}
-    
+
     {!! editor('xe_blog', [
         'content' => Request::old('content'),
         'cover' => true,
@@ -15,10 +15,8 @@
         <input type="text" name="title" value="{{ Request::old('title') }}">
         <input type="text" name="sub_title" value="{{ Request::old('sub_title') }}">
         <input type="text" name="blog_status" value="public">
-        <input type="text" name="background_color" value="{{ Request::old('background_color') }}">
         <input type="text" name="slug" value="{{ Request::old('slug') }}">
         <input type="text" name="published_at" value="{{ Request::old('published_at') }}">
-        <input type="text" name="gallery_group_id" value="{{ Request::old('gallery_group_id') }}">
         <input type="text" name="thumbnail">
         <input type="text" name="cover_image">
         <textarea name="summary">{{ Request::old('summary') }}</textarea>
@@ -80,7 +78,7 @@
 
         <div class="components-panel__body is-opened">
             <h2 class="components-panel__body-title">
-                <button type="button" aria-expanded="true" class="components-button components-panel__body-toggle">카테고리 &amp; 태그</button>
+                <button type="button" aria-expanded="true" class="components-button components-panel__body-toggle">Taxonomy & 태그</button>
             </h2>
             <div class="components-base-control">
                 <div class="components-base-control__field">
@@ -128,84 +126,12 @@
                 </div>
             </div>
         </div>
-
-        <div class="components-panel__body is-opened">
-            <h2 class="components-panel__body-title">
-                <button type="button" aria-expanded="true" class="components-button components-panel__body-toggle">기타</button>
-            </h2>
-            <div class="components-base-control">
-                <div class="components-base-control__field">
-                    <span class="components-base-control__label">배경 컬러</span>
-                    <input type="text" id="__f-background-color" class="components-text-control__input" name="f_background_color" value="{{ Request::old('background_color') }}">
-                </div>
-            </div>
-        </div>
-
-        <div class="components-panel__body is-opened">
-            <h2 class="components-panel__body-title">
-                <button type="button" aria-expanded="true" class="components-button components-panel__body-toggle">갤러리</button>
-            </h2>
-            <div class="components-base-control">
-                <div class="components-base-control__field">
-                    <div class="__f-banner-group">
-                        <input type="hidden" class="__banner-group-id" value="{{ Request::old('gallery_group_id') }}">
-                        <button type="button" class="xe-btn xe-btn-sm __banner-group-create" data-url="{{ route('boldjournal.gallery.store_banner_group') }}">갤러리 생성</button>
-                        <button type="button" class="xe-btn xe-btn-sm __banner-group-edit" style="display:none">편집</button>
-                        <button type="button" class="xe-btn xe-btn-sm __banner-group-delete" style="display:none">삭제</button>
-                    </div>
-                </div>
-            </div>
-        </div>
     </form>
 </div>
 
 <script>
     $(function () {
-        var $bannerGroups = $('.__f-banner-group')
         var $metaboxes = $('#metaboxes')
-        $bannerGroups.each(function () {
-            var $container = $(this)
-            var $field = $container.find('.__banner-group-id')
-            var $btnCreate = $('.__banner-group-create');
-            var $btnEdit = $('.__banner-group-edit');
-            var $btnDelete = $('.__banner-group-delete');
-            var groupId = null;
-
-            if ($field.val()) {
-                groupId = $field.val()
-                $btnCreate.hide()
-                $btnEdit.show()
-                $btnDelete.show()
-            }
-
-            $container.on('click', '.__banner-group-create', function () {
-                var url = $(this).data('url')
-                $btnCreate.hide()
-                XE.post(url).then(function (res) {
-                    $field.val(res.data.groupId)
-                    groupId = res.data.groupId
-                    $metaboxes.find('[name=gallery_group_id]').val(groupId)
-                    $btnEdit.show()
-                    $btnDelete.show()
-                })
-            })
-            $container.on('click', '.__banner-group-edit', function (e) {
-                e.preventDefault()
-                window.open(XE.route('boldjournal.gallery.edit_banner_group', { groupId: groupId } ), 'bannerEditor', 'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes')
-            })
-            $container.on('click', '.__banner-group-delete', function (e) {
-                e.preventDefault()
-                $field.val('')
-                $btnCreate.show()
-                $btnEdit.hide()
-                $btnDelete.hide()
-            })
-            if ($metaboxes.find('[name=gallery_group_id]').val()) {
-                $btnCreate.hide()
-                $btnEdit.show()
-                $btnDelete.show()
-            }
-        })
 
         wp.data.dispatch('core/edit-post').setAvailableMetaBoxesPerLocation({
             "side": [{
@@ -239,7 +165,6 @@
         })
 
         // 폼채우기
-
         $(document).on('change', '#__f-title', function () {
             $metaboxes.find('[name=title]').val($(this).val())
         })
@@ -248,9 +173,6 @@
         })
         $(document).on('change', '#__f-blog-status', function () {
             $metaboxes.find('[name=blog_status]').val($(this).val())
-        })
-        $(document).on('change', '#__f-background-color', function () {
-            $metaboxes.find('[name=background_color]').val($(this).val())
         })
         $(document).on('change', '#__f-slug', function () {
             $metaboxes.find('[name=slug]').val($(this).val())
--- src/Components/Skins/Blog/BlogCommonSkin/views/edit.blade.php
+++ src/Components/Skins/Blog/BlogCommonSkin/views/edit.blade.php
@@ -1,8 +1,6 @@
 {{ XeFrontend::css('plugins/xe_blog/assets/block-editor-dynamic-fields.css')->load() }}
 {{ XeFrontend::js('plugins/xe_blog/assets/js/boldjournal-block-style.js')->load() }}
 
-@expose_route('boldjournal.gallery.edit_banner_group')
-
 <form method="post" action="{{ route('blog.update') }}" enctype="multipart/form-data">
     {!! csrf_field() !!}
     <input type="hidden" name="blogId" value="{{ $blog->id }}">
@@ -26,10 +24,8 @@
         @if ($blog->isTemp() === true)
             <input type="text" name="blog_status" value="temp">
         @endif
-        <input type="text" name="background_color" value="{{ $metaDataHandler->getBackgroundColor($blog) }}">
         <input type="text" name="slug" @if ($blog->slug !== null) value="{{ $blog->slug['slug'] }}" @endif>
         <input type="text" name="published_at" value="{{ $blog->published_at }}">
-        <input type="text" name="gallery_group_id" value="{{ $metaDataHandler->getGalleryGroupId($blog) }}">
         <input type="text" name="thumbnail">
         <input type="text" name="cover_image">
         <textarea name="summary">{{ $metaDataHandler->getSummary($blog) }}</textarea>
@@ -91,7 +87,7 @@
 
         <div class="components-panel__body is-opened">
             <h2 class="components-panel__body-title">
-                <button type="button" aria-expanded="true" class="components-button components-panel__body-toggle">카테고리 &amp; 태그</button>
+                <button type="button" aria-expanded="true" class="components-button components-panel__body-toggle">Taxonomy &amp; 태그</button>
             </h2>
             <div class="components-base-control">
                 <div class="components-base-control__field">
@@ -164,86 +160,12 @@
                 </div>
             </div>
         </div>
-
-        <div class="components-panel__body is-opened">
-            <h2 class="components-panel__body-title">
-                <button type="button" aria-expanded="true" class="components-button components-panel__body-toggle">기타</button>
-            </h2>
-
-
-            <div class="components-base-control">
-                <div class="components-base-control__field">
-                    <span class="components-base-control__label">배경 컬러</span>
-                    <input type="text" id="__f-background-color" class="components-text-control__input" name="f_background_color" value="{{ $metaDataHandler->getBackgroundColor($blog) }}">
-                </div>
-            </div>
-        </div>
-        <div class="components-panel__body is-opened">
-            <h2 class="components-panel__body-title">
-                <button type="button" aria-expanded="true" class="components-button components-panel__body-toggle">갤러리</button>
-            </h2>
-            <div class="components-base-control">
-                <div class="components-base-control__field">
-                    {{-- <span class="components-base-control__label">배경 컬러</span> --}}
-                    <div class="__f-banner-group">
-                        <input type="hidden" class="__banner-group-id" value="{{ $metaDataHandler->getGalleryGroupId($blog) }}">
-                        <button type="button" class="xe-btn xe-btn-sm __banner-group-create" data-url="{{ route('boldjournal.gallery.store_banner_group') }}">갤러리 생성</button>
-                        <button type="button" class="xe-btn xe-btn-sm __banner-group-edit" style="display:none">편집</button>
-                        <button type="button" class="xe-btn xe-btn-sm __banner-group-delete" style="display:none">삭제</button>
-                    </div>
-                </div>
-            </div>
-        </div>
     </form>
 </div>
 
 <script>
     $(function () {
-        var $bannerGroups = $('.__f-banner-group')
         var $metaboxes = $('#metaboxes')
-        $bannerGroups.each(function () {
-            var $container = $(this)
-            var $field = $container.find('.__banner-group-id')
-            var $btnCreate = $('.__banner-group-create');
-            var $btnEdit = $('.__banner-group-edit');
-            var $btnDelete = $('.__banner-group-delete');
-            var groupId = null;
-
-            if ($field.val()) {
-                groupId = $field.val()
-                $btnCreate.hide()
-                $btnEdit.show()
-                $btnDelete.show()
-            }
-
-            $container.on('click', '.__banner-group-create', function () {
-                var url = $(this).data('url')
-                $btnCreate.hide()
-                XE.post(url).then(function (res) {
-                    $field.val(res.data.groupId)
-                    groupId = res.data.groupId
-                    $metaboxes.find('[name=gallery_group_id]').val(groupId)
-                    $btnEdit.show()
-                    $btnDelete.show()
-                })
-            })
-            $container.on('click', '.__banner-group-edit', function (e) {
-                e.preventDefault()
-                window.open(XE.route('boldjournal.gallery.edit_banner_group', { groupId: groupId } ), 'bannerEditor', 'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes')
-            })
-            $container.on('click', '.__banner-group-delete', function (e) {
-                e.preventDefault()
-                $field.val('')
-                $btnCreate.show()
-                $btnEdit.hide()
-                $btnDelete.hide()
-            })
-            if ($metaboxes.find('[name=gallery_group_id]').val()) {
-                $btnCreate.hide()
-                $btnEdit.show()
-                $btnDelete.show()
-            }
-        })
 
         wp.data.dispatch('core/edit-post').setAvailableMetaBoxesPerLocation({
             "side": [{
@@ -273,15 +195,11 @@
         })
 
         // 폼채우기
-
         $(document).on('change', '#__f-sub-title', function () {
             $metaboxes.find('[name=sub_title]').val($(this).val())
         })
         $(document).on('change', '#__f-blog-status', function () {
             $metaboxes.find('[name=blog_status]').val($(this).val())
-        })
-        $(document).on('change', '#__f-background-color', function () {
-            $metaboxes.find('[name=background_color]').val($(this).val())
         })
         $(document).on('change', '#__f-slug', function () {
             $metaboxes.find('[name=slug]').val($(this).val())
